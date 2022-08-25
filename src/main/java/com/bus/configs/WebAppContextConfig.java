e292cc4edf3df9d639348d5c1c2149fa8914839c
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-08-25 15:43:37 +0700

    gitignore

--- src/main/java/com/bus/configs/WebAppContextConfig.java
+++ src/main/java/com/bus/configs/WebAppContextConfig.java
@@ -1,61 +1,87 @@
 package com.bus.configs;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configurable
 @EnableWebMvc
-@ComponentScan(basePackages = {
-		"com.bus"
-})
-public class WebAppContextConfig implements WebMvcConfigurer {
+@ComponentScan("com.bus")
+public class WebAppContextConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		// TODO Auto-generated method stub
 		configurer.enable();
 	}
-	
-	//thymleaf configs
-	@Bean
-	public ClassLoaderTemplateResolver templateResolver() {
-		ClassLoaderTemplateResolver template = new ClassLoaderTemplateResolver();
-		template.setPrefix("classpath:/templates/");
-		template.setTemplateMode("HTML5");
-		template.setSuffix(".html");
-		template.setTemplateMode("XHTML");
-		template.setCharacterEncoding("UTF-8");
-		template.setOrder(1);
-		return template;
-	}
-	
-	@Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource m = new ResourceBundleMessageSource();
-        m.setBasenames("messages");
-        
-        return m;
-    }
 
-    @Override
-    public Validator getValidator() {
-        return validator();
-    }
-    
-    @Bean
-    public Validator validator() {
-        LocalValidatorFactoryBean v = new LocalValidatorFactoryBean();
-        v.setValidationMessageSource(messageSource());
-        
-        return v;
-    }
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	private SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
+
+	// thymleaf configs
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource m = new ResourceBundleMessageSource();
+		m.setBasenames("messages");
+		return m;
+	}
+
+	@Override
+	public Validator getValidator() {
+		return validator();
+	}
+
+	@Bean
+	public Validator validator() {
+		LocalValidatorFactoryBean v = new LocalValidatorFactoryBean();
+		v.setValidationMessageSource(messageSource());
+		return v;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+		this.applicationContext = applicationContext;
+	}
 }
